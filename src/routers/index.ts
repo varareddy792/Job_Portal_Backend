f7a2ead67e200a7e6a010a717e778cdbe7ef4819
srinivas_pamireddy <srinivasreddy.pamireddy@ratnaglobaltech.com>
2023-08-09 12:52:15 +0530

    removed events

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
-import eventsRouter from './events/events';
 import authRouter from './auth/auth';
 
 const router = Router();
-router.use(['/events'], eventsRouter);
+
 router.use(['/auth'], authRouter);
 
 export default router;
