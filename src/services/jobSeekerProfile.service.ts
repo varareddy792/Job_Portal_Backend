199c4f4dc0524c78cc631042ddfb6af2dccef275
Dibyalochan <dibyalochanparida@gmail.com>
2023-08-28 17:26:41 +0530

    added nullable for summery field

--- src/services/jobSeekerProfile.service.ts
+++ src/services/jobSeekerProfile.service.ts
@@ -22,16 +22,15 @@ export const saveJobSeekerProfile = async (jobSeekerParams: Params) => {
 export const updateJobSeekerProfile = async (id: number, jobSeekerParams: JobSeekerProfile) => {
   try {
     const jobSeekerProfileRepository = AppDataSource.getRepository(JobSeekerProfile);
-    const jobSeekerProfile = await jobSeekerProfileRepository.update(id, {
+    const jobSeekerProfile = await jobSeekerProfileRepository?.update(id, {
       ...(jobSeekerParams.completedSections && { completedSections: jobSeekerParams.completedSections }),
       ...(jobSeekerParams.currentLocation && { currentLocation: jobSeekerParams.currentLocation }),
       ...(jobSeekerParams.noticePeriod && { noticePeriod: jobSeekerParams.noticePeriod }),
       ...(jobSeekerParams.profilePicture && { profilePicture: jobSeekerParams.profilePicture }),
       ...(jobSeekerParams.resume && { resume: jobSeekerParams.resume }),
-      ...(jobSeekerParams.profileSummary && { profileSummary: jobSeekerParams.profileSummary }),
+      ...({ profileSummary: jobSeekerParams.profileSummary }),
     });
     return jobSeekerProfile;
-
   } catch (error) {
     console.log('error', error);
     throw error;
